bb236a2aab0ed84ae0bdbed7e85c422888634554
krogenth <kelleydaniel15@gmail.com>
2020-12-14 01:22:16 -0500

    Add files via upload

--- src/formatData.h
+++ src/formatData.h
@@ -3,12 +3,19 @@
 #include <iomanip>
 #include <string>
 #include <sstream>
+#include <iostream>
+
+template<typename CharT>
+struct Sep : public std::numpunct<CharT>
+{
+    virtual std::string do_grouping() const { return "\003"; }
+};
 
 template<class T>
 std::string FormatWithCommas(T value)
 {
     std::stringstream ss;
-    ss.imbue(std::locale("en_US.UTF8"));
+    ss.imbue(std::locale(std::cout.getloc(), new Sep<char>()));
     ss << std::fixed << value;
     return ss.str();
 }
